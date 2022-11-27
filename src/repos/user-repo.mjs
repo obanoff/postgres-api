1542192ae5cd4c4dc1bfda0111609005496b7d57
Yevhen Shliakhovskyi <eugene@Eugenes-MacBook-Air.local>
2022-11-28 01:32:51 +0200

    2nd commit

--- src/repos/user-repo.mjs
+++ src/repos/user-repo.mjs
@@ -1,18 +1,29 @@
 
-import pool from 'pool.mjs';
+import pool from '../pool.mjs';
+import toCamelCase from './utils/to-camel-case.mjs';
 
 export default class UserRepo {
   static async find() {
     try {
       const { rows } = await pool.query('SELECT * FROM users;');
-      return rows;
+
+      return toCamelCase(rows);
     } catch (error) {
       console.error(error.message);
     }
   }
 
-  static async findById() {
+  static async findById(id) {
+    // WARNING: REALLY BIG SECURITY ISSUE!
+    try {
+      const { rows } = await pool.query(`
+        SELECT * FROM users WHERE id = ${id};
+      `);
 
+      return toCamelCase(rows)[0];
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   static async insert() {
--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -5,10 +5,18 @@ import UserRepo from '../repos/user-repo.mjs';
 const router = express.Router();
 
 router.get('/users', async (req, res) => {
-  
+  const users = await UserRepo.find();
+
+  res.send(users);
 });
 
-router.get('/users/:id', async (req, res) => {});
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserRepo.findById(id);
+
+  user ? res.send(user) : res.sendStatus(404);
+});
 
 router.post('/users', async (req, res) => {});
 
